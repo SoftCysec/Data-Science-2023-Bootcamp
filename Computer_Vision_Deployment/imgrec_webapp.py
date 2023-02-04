42af89670b92407d2e00664d275aff375fb70fd8
SoftCysec <softengdedan@gmail.com>
2023-02-04 11:20:24 +0300

    latest commits

--- Computer_Vision_Deployment/imgrec_webapp.py
+++ Computer_Vision_Deployment/imgrec_webapp.py
@@ -1,27 +1,27 @@
-import os
+import os 
 from flask import Flask, request, redirect, url_for, render_template
 from werkzeug.utils import secure_filename
-from keras.models import load_model
-from keras.backend import set_session
-from skimage.transform import resize
-import matplotlib.pyplot as plt
-import tensorflow as tf
-import numpy as np
+
 
 app = Flask(__name__)
 
-#Loading the model
+from keras.models import load_model 
+from keras.backend import set_session
+from skimage.transform import resize 
+import matplotlib.pyplot as plt 
+import tensorflow as tf 
+import numpy as np 
 
+print("Loading model") 
 global sess
 sess = tf.Session()
 set_session(sess)
-global model
-model = load_model('my_cifar10_model.h5')
+global model 
+model = load_model('my_cifar10_model.h5') 
 global graph
 graph = tf.get_default_graph()
 
-
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/', methods=['GET', 'POST']) 
 def main_page():
     if request.method == 'POST':
         file = request.files['file']
@@ -30,7 +30,7 @@ def main_page():
         return redirect(url_for('prediction', filename=filename))
     return render_template('index.html')
 
-@app.route('/prediction/<filename>')
+@app.route('/prediction/<filename>') 
 def prediction(filename):
     #Step 1
     my_image = plt.imread(os.path.join('uploads', filename))
@@ -42,8 +42,9 @@ def prediction(filename):
       set_session(sess)
       probabilities = model.predict(np.array( [my_image_re,] ))[0,:]
       print(probabilities)
-#Step 4
-      number_to_class = ['airplane', 'automobile', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck']
+      #Step 4
+      number_to_class = ['airplane', 'automobile', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 
+'truck']
       index = np.argsort(probabilities)
       predictions = {
         "class1":number_to_class[index[9]],
@@ -53,5 +54,7 @@ def prediction(filename):
         "prob2":probabilities[index[8]],
         "prob3":probabilities[index[7]],
       }
-#Step 5
+    #Step 5
     return render_template('predict.html', predictions=predictions)
+
+app.run(host='0.0.0.0', port=80)
